d5c8bb486409672b1919a082fb016b2045606b4d
saji-ryu <rsajiki@gmail.com>
2019-06-06 17:48:57 +0900

    APIを充実、typeguardを入れる

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -67,29 +67,66 @@ router.get('/', (req: express.Request, res: express.Response) => {
   res.render('index');
 });
 
-router.get(
-  '/:tupleSpaceName',
-  (req: express.Request, res: express.Response) => {
-    if (req.params.tupleSpaceName === '_js') {
-      // 配布する静的ファイルの説明ページ
-      res.render('jsIndex');
-    } else {
-      res.render('tupleSpace', {
-        tupleSpaceName: req.params.tupleSpaceName,
-        watchTuple: req.query,
-      });
-    }
-  },
-);
+// router.get(
+//   '/:tupleSpaceName',
+//   (req: express.Request, res: express.Response) => {
+//     if (req.params.tupleSpaceName === '_js') {
+//       // 配布する静的ファイルの説明ページ
+//       res.render('jsIndex');
+//     } else {
+//       res.render('tupleSpace', {
+//         tupleSpaceName: req.params.tupleSpaceName,
+//         watchTuple: req.query,
+//       });
+//     }
+//   },
+// );
+
+const isValidLindaOperation = (operation: any): operation is LindaOperation => {
+  const keys = Object.keys(operation);
+  if (!operation) return false;
+  if (
+    !keys.includes('_payload') ||
+    !keys.includes('_where') ||
+    !keys.includes('_type')
+  ) {
+    return false;
+  }
+  return (
+    typeof operation._where === 'string' &&
+    ['read', 'write', 'take'].includes(operation._type)
+  );
+};
 
 router.post(
   '/:tupleSpaceName',
   (req: express.Request, res: express.Response) => {
     const linda: Linda = app.get('linda');
     let ts = linda.tupleSpace(req.params.tupleSpaceName);
-    ts.write(req.body, resData => {
-      res.send({ status: 'ok', tuple: resData });
-    });
+    if (isValidLindaOperation(req.body)) {
+      switch (req.body._type) {
+        case 'write':
+          ts.write(req.body, resData => {
+            res.send({ status: 'ok', resData });
+          });
+          break;
+        case 'read':
+          ts.read(req.body, resData => {
+            res.send({ status: 'ok', resData });
+          });
+          break;
+        case 'take':
+          ts.take(req.body, resData => {
+            res.send({ status: 'ok', resData });
+          });
+          break;
+        default:
+          res.send({ status: 'invalid operation' });
+          break;
+      }
+    } else {
+      res.send({ status: 'invalid operation' });
+    }
   },
 );
 
